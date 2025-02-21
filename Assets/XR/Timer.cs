957bfa7a97e37bbaff2651ca2cf54c7796506717
Helen McKay <helen.mckay@comcast.net>
2025-02-21 01:22:22 -0600

    added some getters and setters, updated comments

--- Assets/XR/Timer.cs
+++ Assets/XR/Timer.cs
@@ -1,44 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 // Made by: Helen McKay | 2102556
 
 // notes:
-    // all variables are private
+    // all variables are private, some use properties (C#'s getters and setters)
     // variables that need to be accessed in Unity have [SerializeField]
-    // if any of these variables later need to be accessed or updated elsewhere in the project, can use properties (C#'s getters and setters)
-        // will only set those up when they're needed
     // made a function for handling timer reaching 0 (if it's not needed, can remove the function)
 
 // usage:
     // attach this script to an object
     // connect a Text Mesh Pro (TMP) object to the TMP_Text field timeText
-    // feel free to chage the value of INITIAL_TIME_REMAINING to whatever is needed for the scene
+    // initialTimeRemaining is set by the GameLoop, feel free to set it to be whatever
     // feel free to add [SerializeField] to more variables as desired
 
 public class Timer: MonoBehaviour
 {
-    // consts
-
-    [SerializeField]
-    private float INITIAL_TIME_REMAINING = 180f;    // the value the timer starts at, serializable so can edit its value in Unity
-
     // variables
-    
-    private float rawTimeRemaining;                 // the time remaining, as a float
-    private bool isTimerRunning;                    // boolean check of whether the timer is still running
-    [SerializeField]
-    private TMP_Text timeText;                      // Text Mesh Pro object that displays the time
-    private bool didTimeRunOut;                     // boolean check of whether timer reached zero (meaning, player did not solve puzzle in time)
 
+    [SerializeField]
+    private float initialTimeRemaining;
+    public float InitialTimeRemaining {get {return initialTimeRemaining;} set {initialTimeRemaining = value;}}  // the value the timer starts at
+
+    private float rawTimeRemaining;                                                                             // the time remaining, as a float
+    private bool isTimerRunning;
+    public bool IsTimerRunning {get {return isTimerRunning;}}                                                   // boolean check of whether the timer is still running
+    
+    [SerializeField]
+    private TMP_Text timeText;                                                                                  // Text Mesh Pro object that displays the time                   
+    private bool didTimeRunOut;
+    public bool DidTimeRunOut{get {return didTimeRunOut;}}                                                      // boolean check of whether timer reached zero (meaning, player did not solve puzzle in time)
+    
     // getFormattedTime
         // formats the time in minutes and seconds
         // receives time as a float
         // returns formatted time as a string
-    private string getFormattedTime(float the_time)
+    private string GetFormattedTime(float the_time)
     {
         float minutes = Mathf.FloorToInt(the_time / 60);
         float seconds = Mathf.FloorToInt(the_time % 60);
@@ -46,19 +48,24 @@ public class Timer: MonoBehaviour
         return string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
+    public void StopTheTimer()
+    {
+        isTimerRunning = false;
+    }
+
     // handlers
 
-    private void handleTimeRunOut()
+    private void HandleTimeRunOut()
     {
         // FIXME: handle time run out
     }
 
     // before first frame
         // initialize variables
-    private void Start()
+    public void Start()
     {
-        rawTimeRemaining = INITIAL_TIME_REMAINING;
-        timeText.text = getFormattedTime(rawTimeRemaining);
+        rawTimeRemaining = InitialTimeRemaining;
+        timeText.text = GetFormattedTime(rawTimeRemaining);
         isTimerRunning = true;
         didTimeRunOut = false;
     }
@@ -66,24 +73,24 @@ public class Timer: MonoBehaviour
     // every frame
     void Update()
     {
-        if (isTimerRunning)
+        if (IsTimerRunning)
         {
             if (rawTimeRemaining > 0)
             {
                 rawTimeRemaining -= Time.deltaTime;
-                timeText.text = getFormattedTime(rawTimeRemaining);
+                timeText.text = GetFormattedTime(rawTimeRemaining);
             }
             else
             {
                 rawTimeRemaining = 0;
-                timeText.text = getFormattedTime(0);
+                timeText.text = GetFormattedTime(0);
                 isTimerRunning = false;
                 didTimeRunOut = true;
             }
         }
-        else if (didTimeRunOut)
+        else if (DidTimeRunOut)
         {
-            handleTimeRunOut();
+            HandleTimeRunOut();
         }
     }
 }
