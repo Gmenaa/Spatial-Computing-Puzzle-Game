2e1079bf476acba2ea77e5806d698b15514c557b
Kian Chavoshi <kiantheprogrammer@gmail.com>
2025-03-18 23:22:30 -0500

    chore: hanoi tower optimization

--- Assets/HanoiTowerAssets/HanoiGameManager.cs
+++ Assets/HanoiTowerAssets/HanoiGameManager.cs
@@ -9,55 +9,14 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public TMP_Text winMessageText;
-    public GameObject[] rods;
-    public int totalDisks = 4;
-
-    void Start()
+    
+    private void Start()
     {
-        if (winMessageText != null)
-            winMessageText.gameObject.SetActive(false);
+        winMessageText.gameObject.SetActive(false);
     }
 
-    void Update()
+    public void HandleWin()
     {
-        CheckWinCondition();
-    }
-
-    void CheckWinCondition()
-    {
-        foreach (GameObject rod in rods)
-        {
-            bool rodIsCorrect = true;
-            for (int pos = 1; pos <= totalDisks; pos++)
-            {
-                string positionName = rod.name + "diskPosition" + pos;
-                Transform posTransform = rod.transform.Find(positionName);
-                if (posTransform == null)
-                {
-                    rodIsCorrect = false;
-                    break;
-                }
-                if (posTransform.childCount == 0)
-                {
-                    rodIsCorrect = false;
-                    break;
-                }
-                GameObject disk = posTransform.GetChild(0).gameObject;
-                string expectedDiskName = "SquareHanoiDisk" + (totalDisks - pos + 1);
-                if (disk.name != expectedDiskName)
-                {
-                    rodIsCorrect = false;
-                    break;
-                }
-            }
-            if (rodIsCorrect)
-            {
-                if (winMessageText != null)
-                    winMessageText.gameObject.SetActive(true);
-                return;
-            }
-        }
-        if (winMessageText != null)
-            winMessageText.gameObject.SetActive(false);
+        winMessageText.gameObject.SetActive(true);
     }
 }
---
+++ Assets/HanoiTowerAssets/RodGameManagerHelper.cs
@@ -0,0 +1,77 @@
+using Oculus.Interaction;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RodGameManagerHelper : MonoBehaviour
+{
+    [SerializeField] private GameManager _gameManager;
+    [SerializeField] private GameObject rod;
+    private SnapInteractor _currentInteractor;
+    private List<int> diskSizes = new List<int>();
+    
+    private void HandleInteractorAdded(SnapInteractor interactor)
+    {
+        diskSizes.Clear();
+        
+        for (int i = 0; i < rod.transform.childCount; i++)
+        {
+            Transform childTransform = rod.transform.GetChild(i);
+            SnapInteractable snapPos = childTransform.GetComponent<SnapInteractable>();
+
+            if (snapPos != null && snapPos.SelectingInteractors.Any())
+            {
+                SnapInteractor diskInteractor = snapPos.Interactors.FirstOrDefault();
+                if (diskInteractor != null)
+                {
+                    GameObject disk = diskInteractor.transform.parent.gameObject;
+                    HanoiDisk hanoiDisk = disk.GetComponent<HanoiDisk>();
+                    diskSizes.Add(hanoiDisk.diskSize);
+                }
+            }
+        }
+
+        if (diskSizes.Count == 4 && IsOrderedAscending(diskSizes))
+        {
+            _gameManager.HandleWin();
+        }
+    }
+
+    private bool IsOrderedAscending(List<int> sizes)
+    {
+        for (int i = 1; i < sizes.Count; i++)
+        {
+            if (sizes[i] <= sizes[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private void OnEnable()
+    {
+        foreach (Transform child in rod.transform)
+        {
+            SnapInteractable snapPos = child.GetComponent<SnapInteractable>();
+            if (snapPos != null)
+            {
+                snapPos.WhenInteractorAdded.Action += HandleInteractorAdded;
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (rod == null) return;
+        
+        foreach (Transform child in rod.transform)
+        {
+            SnapInteractable snapPos = child.GetComponent<SnapInteractable>();
+            if (snapPos != null)
+            {
+                snapPos.WhenInteractorAdded.Action -= HandleInteractorAdded;
+            }
+        }
+    }
+}
