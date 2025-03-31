917b2ce6e3a9d3083b496d6a1c2aa2e847fce493
Hannadem12 <hannadasafw@gmail.com>
2025-03-31 10:37:15 -0500

    UI without scenes

---
+++ Assets/UI scripts/introman3.cs
@@ -0,0 +1,18 @@
+/*using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class introman3 : MonoBehaviour
+{
+    public float introDuration = 3f; 
+
+    void Start()
+    {
+        Invoke("LoadpipesScene", introDuration);
+    }
+
+    void LoadpipesScene()
+    {
+        SceneManager.LoadScene("pipes"); 
+    }
+}
+/*
