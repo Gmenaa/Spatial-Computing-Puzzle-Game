996f3abbbcae823e745b1536f528696e12c76490
Hanna Asfaw <hannadasfaw@gmail.com>
2025-04-08 22:54:12 -0500

    restorationpart17

---
+++ Assets/UI scripts/IntroManager.cs
@@ -0,0 +1,18 @@
+/*using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    public float introDuration = 3f; 
+
+    void Start()
+    {
+        Invoke("LoadBallMazeScene", introDuration);
+    }
+
+    void LoadBallMazeScene()
+    {
+        SceneManager.LoadScene("BallMaze"); 
+    }
+}
+*/
---
+++ Assets/UI scripts/introman4.cs
@@ -0,0 +1,18 @@
+/*using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class introman4 : MonoBehaviour
+{
+    public float introDuration = 3f; 
+
+    void Start()
+    {
+        Invoke("LoadHanoiTowerScene", introDuration);
+    }
+
+    void LoadHanoiTowerScene()
+    {
+        SceneManager.LoadScene("HanoiTower"); 
+    }
+}
+*/
